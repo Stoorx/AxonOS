d5bbae2877cfd4e1ea24981d686c01ef6cfb5e4b
Stoorx <antiproton2013@yandex.ru>
2019-06-02 00:49:56 +0300

    Add Types

---
+++ Kernel/Include/Types.h
@@ -0,0 +1,37 @@
+//
+// Created by Stoorx on 02.06.2019.
+//
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// void
+typedef void Void, * VoidPtr;
+
+// 1 byte
+typedef char SByte, * SBytePtr;
+typedef unsigned char Byte, * BytePtr;
+
+// 2 bytes
+typedef short Short, * ShortPtr;
+typedef unsigned short UShort, * UShortPtr;
+
+// 4 bytes
+typedef int Int, * IntPtr;
+typedef unsigned int UInt, * UIntPtr;
+
+// 8 bytes
+typedef long long Long, * LongPtr;
+typedef unsigned long long ULong, * ULongPtr;
+
+// Floating point
+typedef float Float, * FloatPtr;
+typedef double Double, * DoublePtr;
+typedef long double LongDouble, * LongDoublePtr;
+
+#ifdef __cplusplus
+};
+#endif
