c42ea25399533b9e287b575f73b925d533d35aec
Stoorx <antiproton2013@yandex.ru>
2019-08-01 23:47:44 +0300

    Boot/AxonBoot: Add Startup entry point

---
+++ Boot/AxonBoot/x86/Startup.asm
@@ -0,0 +1,106 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  AxonBoot bootloader                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Memory map                          ;;
+;;                   physical address   ;;
+;;  +------------------------+ 00000H   ;;
+;;  | Interrupt Vector Table |          ;;
+;;  +------------------------+ 00400H   ;;
+;;  |     BIOS Data Area     |          ;;
+;;  +------------------------+ 00500H   ;;
+;;  | PrtScr Status / Unused |          ;;
+;;  +------------------------+ 00600H   ;;
+;;  |       VBR (512 B)      |          ;;
+;;  +------------------------+ 00800H   ;;
+;;  |      VBR II (512 B)    |          ;;
+;;  +------------------------+ 00A00H   ;;
+;;  |    VBR stack (512 B)   |          ;;
+;;  +------------------------+ 00C00H   ;;
+;;  |    Bootloader stack    |          ;;
+;;  +------------------------+ 01000H   ;;
+;;  |   BootLoader (508 KB)  |          ;;
+;;  +------------------------+ 80000H   ;;
+;;  |    Available Memory    |          ;;
+;;  +------------------------+ 9FC00H   ;;
+;;  |          EBDA          |          ;;
+;;  +------------------------+ A0000H   ;;
+;;  |    Video RAM & ROM     |          ;;
+;;  +------------------------+ FFFFFH   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%define BOOT_ADDR 0x1000
+%define LOADER_STACK_16 0x1000
+%define LOADER_STACK_32 0x80000
+
+;[SECTION .text16]
+[ORG BOOT_ADDR]
+[BITS 16]
+start:
+    ; Context:
+    ; DL = drive number
+    ; CS=0x0, IP = 0x1000
+
+    ; init registers
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, LOADER_STACK_16
+
+    in al, 0x92
+    test al, 2
+    jnz a20_enabled
+    or al, 2
+    and al, 0xFE
+    out 0x92, al
+    a20_enabled:
+
+    cli
+
+    ; Set PE flag
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
+
+    push dword GDT
+    push word (GDT.end - GDT - 1)
+    mov bp, sp
+    lgdt [bp]
+
+    jmp 0x8:.protected_entry
+
+[BITS 32]
+.protected_entry:
+    mov ax, 16 ; Data selector
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov dword [0xB8000], 0x07320733 ; Prints "32"
+    ; TODO: initialize protected stack and C-decl function call
+hlt_system:
+    cli
+    hlt
+    jmp short hlt_system
+
+
+;[SECTION .data16]
+GDT:
+.null_desctiptor:
+    dd 0x0
+    dd 0x0
+.code32_descriptor:
+    dw 0xFFFF ; Limit
+    dw 0x0 ; Base
+    db 0x0 ; Base
+    db 0x9A ; Type: Present, DPL0, Code, Readable
+    db 0xCF ; Granularity in pages, 32 bit
+    db 0x0 ; Base
+.data32_descriptor:
+    dw 0xFFFF ; Limit
+    dw 0x0 ; Base
+    db 0x0 ; Base
+    db 0x92 ; Type: Present, DPL0, Data, Writeable
+    db 0xCF ; Granularity in pages, 32 bit
+    db 0x0 ; Base
+.end:
+
